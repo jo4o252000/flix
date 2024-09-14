851fd30d7e29a78dcdae7c9f76efb9e4f02fcd44
jo4o252000 <joaovictor252000@gmail.com>
2024-09-13 23:10:30 -0300

    feat:add middleware errorhandler

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import  express  from "express";
 import { videoRoutes } from "./interface/routes/videoRoutes";
-import cors from 'cors'
+import cors from 'cors';
+import {errorHandler} from './interface/middleware/errorHandler';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors({origin:"*"}));
 app.use("/api", videoRoutes);
+app.use(errorHandler)
 
-const PORT = 4010;
+const PORT = process.env.PORT_API;
 
 app.listen(PORT, () => {
     console.log(`servidor rodando no enderaço http://localhost:${PORT}`)
